107e84f9d2abe4baedf530c5d54ed30c18a50536
unknown <waguinhofilhow@gmail.com>
2025-08-26 14:07:27 -0300

    Update main class to execute only 1 input

--- src/Teste.java
+++ src/Teste.java
@@ -8,31 +8,25 @@ import sintatic.*;
 
 public class Teste {
     public static void main(String[] args) {
-        // Diretórios conforme nova estrutura
-        Path pastaEntrada = Path.of("entradas");
+        Path pastaEntrada = Path.of("input");
 
-        for (int i = 1; i <= 6; i++) {
-            try {
-                // Caminho do arquivo de entrada
-                Path caminhoEntrada = pastaEntrada.resolve("exemplo" + i + ".txt");
+        try {
+            // Caminho do arquivo de entrada
+            Path caminhoEntrada = pastaEntrada.resolve("input.txt");
 
-                // Lê o conteúdo do arquivo
-                String codigoFonte = Files.readString(caminhoEntrada);
+            // Lê o conteúdo do arquivo
+            String codigoFonte = Files.readString(caminhoEntrada);
 
-                System.out.println("Execução do exemplo " + i + ":");
 
-                // Executa o analisador léxico
-                Lexer lexer = new Lexer(codigoFonte);
-                List<Token> tokens = lexer.getTokens();
+            // Executa o analisador léxico
+            Lexer lexer = new Lexer(codigoFonte);
+            List<Token> tokens = lexer.getTokens();
 
-                Parser parser = new Parser(tokens);
-                parser.parseProgram();
+            Parser parser = new Parser(tokens);
+            parser.parseProgram();
 
-                System.out.println("\nExecução com sucesso do exemplo "+i+"\n");
-
-            } catch (Exception e) {
-                System.err.println("Erro ao processar exemplo" + i + ".txt: " + e.getMessage() + "\n");
-            }
+        } catch (Exception e) {
+            System.err.println("Erro ao processar input:" + e.getMessage() + "\n");
         }
     }
     
